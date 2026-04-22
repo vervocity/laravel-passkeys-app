944ad920bcd20bcdce3aeedd9e34d4934537a686
Joshua Bixler <jbixler@vervocity.io>
2026-04-22 15:02:23 -0500

    Refactor WebAuthn functions to normalize options for registration and authentication

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
 import {browserSupportsWebAuthn, startRegistration, startAuthentication, } from "@simplewebauthn/browser";
 
-window.startRegistration = startRegistration;
+const normalizeWebAuthnOptions = (options) => options?.optionsJSON ?? options;
+
+window.startRegistration = (options) => startRegistration(normalizeWebAuthnOptions(options));
 window.browserSupportsWebAuthn = browserSupportsWebAuthn;
-window.startAuthentication = startAuthentication;
+window.startAuthentication = (options, useBrowserAutofill = false) =>
+	startAuthentication(normalizeWebAuthnOptions(options), useBrowserAutofill);
